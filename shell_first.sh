b4ba32e539348df95bdf2eca6254d2f7a7de3e98
AprilWang93 <mwang@cbtnuggets.com>
2018-07-06 13:57:42 -0700

    shell first

---
+++ shell_first.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# declare STRING variable
+STRING="Hello World"
+#print variable on a screen
+echo $STRING
+
+#Define bash global variable
+#This variable is global and can be used anywhere in this bash script
+VAR="global variable"
+function bash {
+#Define bash local variable
+#This variable is local to bash function only
+local VAR="local variable"
+echo $VAR
+}
+echo $VAR
+bash
+# Note the bash global variable did not change
+# "local" is bash reserved word
+echo $VAR
