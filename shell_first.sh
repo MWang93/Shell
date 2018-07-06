4c30417a65b8422f3133b56bfe78bd828ecacc95
AprilWang93 <mwang@cbtnuggets.com>
2018-07-06 15:08:16 -0700

    few tests

--- shell_first.sh
+++ shell_first.sh
@@ -18,3 +18,16 @@ bash
 # Note the bash global variable did not change
 # "local" is bash reserved word
 echo $VAR
+
+#Reading user input
+echo -e "Can you please enter two words? "
+read word1 word2
+echo "Here is your input: \"$word1\" \"$word2\""
+# read command now stores a reply into the default build-in variable $REPLY
+echo -e "How do you feel about bash scripting? "
+read
+echo "You said $REPLY, I'm glad to hear that! "
+echo -e "What are your favorite colours ? "
+# -a makes read command to read into an array
+read -a colours
+echo "My favorite colours are also ${colours[0]}, ${colours[1]} and ${colours[2]}:-)" 
