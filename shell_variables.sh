a2c304470bf1b4c3fcd97743bae73cf04d6d59bd
MWang93 <40410176+MWang93@users.noreply.github.com>
2018-07-12 16:43:41 -0700

    Add files via upload

---
+++ shell_variables.sh
@@ -0,0 +1,30 @@
+# Create a Simple welcome
+greeting="Welcome"
+user=$(whoami)
+day=$(date +%A)
+
+echo "$greeting back $user! Today is $day, which is the best day of the entire week!"
+echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
+
+# Single Quotes versus double quotes
+## Basically, variable names are exapnded within double quotes, but not single quotes. 
+## If you do not need to refer to variables, single quotes are good to use as the results are more predictable.
+echo -n '$USER='
+echo "$USER"
+echo "\$USER=$USER"
+
+# Using Quotes to enclose your variables
+## Sometimes, it is a good idea to protect variable names in double quotes. 
+## This is usually the most important if your variables value either (a) contains spaces or (b) is the empty string
+X=""
+if [ -n $X ]; then 	# -n tests to see if the argument is non empty
+	echo "first: the variable X is not the empty string"
+fi
+X=""
+if [ -n "$X" ]; then 	# -n tests to see if the argument is non empty
+	echo "second: the variable X is not the empty string"
+fi
+
+# Using Braces to Protect Your Variables
+X=ABC
+echo "${X}abc"
