7d365ae76995219384a16dc187385dc80da3d965
MWang93 <40410176+MWang93@users.noreply.github.com>
2018-09-04 13:44:40 -0700

    Add files via upload

--- shell_loop.sh
+++ shell_loop.sh
@@ -3,3 +3,29 @@ for i in 1 2 3 4 5
 do
    echo "Welcome $i times"
 done
+
+#!/bin/bash
+c=1
+while [ $c -le 5 ]
+do 
+	echo "Welcome $c times"
+	((c++)) #c=$(( c+1 ))
+done
+
+#!/bin/bash
+file=/Users/mwang/documents/shell/resolv.txt
+while IFS= read -r line
+do
+	printf '%s\n' "$line" #echo "$line"
+done < "$file"
+
+#!/bin/bash
+filee=/Users/mwang/documents/shell/foo.txt
+regex=".* bird .*"
+while IFS= read -r line
+do 
+	if [[ $line =~ $regex ]]
+	then 
+		echo $line 
+	fi
+done < "$filee"
